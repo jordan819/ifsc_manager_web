431b7f7dbe7eb16aab9b53b685e1605790edf5a4
jordan819 <patryk.zaucha00@gmail.com>
2023-01-25 03:14:48 +0100

    update climber item display

--- src/Pages/Climbers.js
+++ src/Pages/Climbers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import {Card, Col} from "react-bootstrap";
 
 export default class Climbers extends React.Component {
     state = {
@@ -16,20 +17,24 @@ export default class Climbers extends React.Component {
 
     render() {
         return (
-            <ul>
-                {
-                    this.state.climbers
-                        .map(climber =>
-                            <li key={climber.id}>
-                                Id: {climber.id} <br/>
-                                Imię i nazwisko: {climber.name + " "} <br/>
-                                Rok urodzenia: {climber.yearOfBirth} <br/>
-                                Kraj pochodzenia: {climber.country} <br/>
-                                Federacja: {climber.federation} <br/>
-                            </li>
-                        )
-                }
-            </ul>
+            this.state.climbers
+                .map(climber =>
+                    <Col className="insure-block">
+                        <Card>
+                            <Card.Body>
+                                <Card.Link href="#" style={{height: '100'}}> <Card.Img
+                                    src={climber.image}/>
+                                    <Card.Title>{climber.name} ({climber.id})</Card.Title>
+                                    <Card.Text>
+                                        Rok urodzenia: {climber.yearOfBirth}<br/>
+                                        Kraj pochodzenia: {climber.country}<br/>
+                                        Federacja: {climber.federation}
+                                    </Card.Text>
+                                </Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )
         )
     }
 }
