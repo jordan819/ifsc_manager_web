726d7f0885b2cd9f595638a9a06a97091bce87ac
jordan819 <patryk.zaucha00@gmail.com>
2023-02-06 17:03:10 +0100

    refactor

--- src/Pages/ErrorPage.js
+++ src/Pages/ErrorPage.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function ErrorPage() {
     return (
-        <div>ErrorPage</div>
+        <div className="container my-5">
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-12">
+                    <div className="alert alert-danger" role="alert">
+                        <h4 className="alert-heading">Error!</h4>
+                        <p>Przepraszamy, coś poszło nie tak. Proszę spróbować ponownie później.</p>
+                        <p className="mb-0">Jeśli problem nadal występuje, skontaktuj się z pomocą techniczną.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
